c8783e0ecbf71bc18a167df513b8a66802ee6eb8
livelovebll <70596067+livelovebll@users.noreply.github.com>
2022-07-28 10:24:06 -0700

    get route for mainPage

---
+++ server.js
@@ -0,0 +1,18 @@
+const {application} = require("express");
+const express = require("express");
+const { resolve } = require("node:path");
+const path = require ("node:path");
+const api = require("./routes/api")
+
+const PORT = process.env.port || 3001;
+const app = express();
+
+app.use(express.json());
+
+app.use("api/notes", api);
+
+app.use(express.static("public"));
+
+
+
+app.get("/", (require, resolve) => resolve
